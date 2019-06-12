3cf233e38ed6bdc69a01eef069cb3028258a9446
chromatyk <42806308+chromatyk@users.noreply.github.com>
2019-06-12 15:55:56 +0200

    complete v1

--- index.php
+++ index.php
@@ -1,208 +1,171 @@
-<!doctype html>
-<html lang="fr">
+<!DOCTYPE html>
+<html>
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-  <meta name="description" content="" />
+  <meta name="description" content="Kaiartsu est une agence web basée en Hauts-de-France. Spécialisée dans le développement de site web et d'identité visuelle." />
   <meta name="publisher" content="Kaiartsu">
   <meta name="author" content="Kaiartsu" />
-  <meta name="reply-to" content="contact@kaiartsu.fr">
-  <meta name="keywords" content="" />
-  <title>Kaiartsu | L'agence web symbiotique | Création de sites web | Identité visuelle | Hauts-de-France, Noyon</title>
-  <!-- Link CSS -->
-  <link href="assets/lib/bootstrap/css/bootstrap.css" rel="stylesheet" />
-  <link href="assets/css/main.css" rel="stylesheet" />
-  <link href="assets/css/mediaAtelier.css" rel="stylesheet" />
-  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  <meta name="reply-to" content="eloise.couteau@paysnoyonnais.fr">
+  <meta name="keywords" content="Assises du Noyonnais ; Noyon ; Campus Inovia ; 27 Juin ; Pays Noyonnais ; CCPN ; Communauté de commune du pays Noyonnais" />
+  <title>Soirée de lancement des assises du Noyonnais | 27 Juin 2019 | Campus Inovia</title>
+  <link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css" />
+  <link href="assets/css/animate.css" rel="stylesheet" type="text/css" />
+  <link href="assets/css/main.css" rel="stylesheet" type="text/css" />
+  <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 </head>
 <body>
-  <nav class="navbar navbar-expand-lg navbar-light">
-    <a class="navbar-brand" href="#"><img src="assets/images/logo.jpg" /></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Thématiques</a>
-        </li>
+  <main>
+    <video autoplay loop poster="assets/images/imageFixe.jpg" id="bgvid">
+      <source src="assets/video/actFond.webm" type="video/webm">
+        <source src="assets/video/actFond.mp4" type="video/mp4">
+        </video>
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-sm-12 col-md-6 col-xl-6 firtsPart">
+              <div class="row">
+                <div class="col-12">
+                  <img id="logo" src="assets/images/logoTransV2.png">
+                </div>
+              </div>
+              <div class="row">
+                <div id="bands" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                  <div class="row">
+                    <div class="col-xl-12">
+                      <p class="titleNight">Les assises du Noyonnais</p>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-12">
+                      <hr/>
+                    </div>
+                  </div>
+                  <div class="row lineBox">
+                    <div id="inboxText" class=" col-12 ">
+                      <a href="themes.php" title="Thématiques">Thématiques</a>
+                      <a href="question.php" title="Questionnaire">Questionnaire</a>
+                      <a href="ateliers.php" title="Ateliers">Ateliers</a>
+                      <a href="contact.php" title="Contact">Contact</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-12 col-md-6 col-xl-6 secondPart">
+              <div class="row firstSection">
+                <div  class=" col-12">
+                  <div class="row">
+                    <div class="col-12">
+                      <h1>Assises du Noyonnais 2019</h1>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-12">
+                      <p class="lorem">La Communauté de communes du Pays noyonnais est un territoire de 35 000 habitants répartis sur 42 communes. Son histoire économique, récente, est marquée par le départ de grands employeurs historiques (Intersnack, Brézillon, Jacob Delafon…) et d’un régiment militaire (1200 militaires).</div>
+                  </div>
+                  <div class="row">
+                    <div class="col-12">
+                      <p class="lorem">La Communauté de communes du Pays noyonnais a alors dû prendre des engagements ambitieux afin d’accompagner la mutation de son territoire (création d’un campus économique par la signature d’un Contrat de Redynamisation de Sites de Défense, soutien au canal Seine nord Europe, aide à l’implantation d’entreprises…).</p>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-12">
+                      <p class="lorem">Afin de poursuivre ces efforts de dynamisation du territoire, la Communauté de communes du Pays noyonnais lance <span id="boldUnderline">des Assises de l’économie et de l’emploi</span> à l’échelle intercommunale. Cette grande consultation associe toutes les parties prenantes du territoire noyonnais (porteurs de projets, entreprises, commerçants, salariés, associations, habitants, communes, etc.).</p>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-12">
+                      <p class="lorem">7 grandes thématiques seront abordées lors de ces assises de l’économie et de l’emploi :</p>
+                      <ul>
+                        <li>L’offre d’accueil des entreprises </li>
+                        <li>L’emploi et la formation</li>
+                        <li>La mobilité</li>
+                        <li>La place du campus Inovia dans le développement économique du territoire </li>
+                        <li>Le commerce et les services</li>
+                        <li>Le numérique dans l’économie territoriale</li>
+                        <li>Comment anticiper le grand chantier du projet CSNE</li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-12">
+                      <p class="lorem">Afin de permettre au plus grand nombre de s’exprimer sur son territoire, les assises du noyonnais se déclineront sous deux modes de consultation : </p>
+                      <ul>
+                        <li>L’offre d’accueil des entreprises </li>
+                        <li>L’emploi et la formation</li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-12">
+                      <p class="lorem">La Communauté de communes du Pays noyonnais a ainsi l’ambition affichée d’améliorer l’attractivité et la compétitivité de son territoire.  </p>
 
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Ateliers</a>
-        </li>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-12">
+                      <p class="lorem">Cette consultation permettra d’écrire les premières lignes de son Schéma Territorial de Développement Economique (STDE), afin de fixer des objectifs ambitieux mais atteignables dont la finalité est le retour de la croissance et de l’emploi dans les années à venir. </p>
 
-        <li class="nav-item">
-          <a class="nav-link" href="#">Questionnaire</a>
-        </li>
-      </ul>
-      <a class="link" href="#">Contact</a>
-    </div>
-  </nav>
-  <main id="">
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-xs-6 col-sm-5 col-md-5 col-lg-5 col-xl-5" id="breadCrumb">
-          <p>
-            Accueil \ Ateliers
-          </p>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-12">
-          <h1>Ateliers des assises de l'économie et<br> de l'emploi du noyonnais</h1>
-        </div>
-      </div>
-      <div class="row atelierLine">
-        <div class="col-xs-12 col-lg-6 ">
-          <div class="atelierCard">
-              <div id="parallelogramPurR">
-                <div class="row textAtelier">
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-12">
+                      <p class="lorem">A compter du 28 juin, retrouvez sur le site www.assisesdunoyonnais.fr : </p>
+                      <ul>
+                        <li>Le questionnaire en ligne des Assises du noyonnais</li>
+                        <li>Les 7 thématiques de travail qui seront menées pendant les Assises du noyonnais</li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-12">
+                      <p class="lorem"></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="thirdSection">
+                <div class="row partnerBox">
                   <div class="col-12">
-                    <p class="atelier">ATELIER #1</p>
+                    <h2>Partenaires</h2>
+                  </div>
+                  <div class="col-xs-12 col-sm-6 col-md-12 col-lg-6 col-xl-4">
+                    <img class="imgPartner" src="assets/images/hdf.jpg">
+                  </div>
+                  <div class="col-xs-12 col-sm-6 col-md-12 col-lg-6 col-xl-4">
+                    <img class="imgPartner" src="assets/images/ccpn.jpg">
+                  </div>
+                  <div class="col-xs-12 col-sm-6 col-md-12 col-lg-6 col-xl-4">
+                    <img class="imgPartner" src="assets/images/noyonLogo.jpg">
+                  </div>
+                                  </div>
+                <div id="creditsBox">
+                <div class="row">
+                  <div class="col-12">
+                    <p class="credits">Vidéo réalisée par <a href="https://dronedecole.fr/">Drône d'école</a></p>
                   </div>
                 </div>
-                <div class="row textAtelier">
-                  <div class="col-xs-12 col-sm-12 col-md-12 col-lg-9 col-xl-9">
-                    <p class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                  </div>
-                </div>
-                <div class="row linkSubscribeLine">
-                  <div class="col-12 linkSubscribe">
-                    <a href="#" class="">Inscrivez-vous</a>
+                <div class="row">
+                  <div class="col-12">
+                    <p class="credits">Site réalisé par <a href="https://www.kaiartsu.fr">Kaiartsu</a></p>
                   </div>
                 </div>
               </div>
-              <div id="imageRight">
               </div>
+            </div>
           </div>
         </div>
-        <div class="col-xs-12 col-lg-6 ">
-          <div class="atelierCard">
-              <div id="parallelogramOrL">
-                <div class="row textAtelierR">
-                  <div class="col-12">
-                    <p class="atelierR">ATELIER #2</p>
-                  </div>
-                </div>
-                <div class="row textAtelierR">
-                  <div class="col-10">
-                    <p class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                  </div>
-                </div>
-                <div class="row linkSubscribeLine">
-                  <div class="col-12 linkSubscribeR">
-                    <a href="#" class="">Inscrivez-vous</a>
-                  </div>
-                </div>
-              </div>
-              <div id="imageRight">
-              </div>
+      </main>
+      <footer>
+        <div class="row footer">
+          <div class="col-12">
+
           </div>
         </div>
-      </div>
-      <div class="row atelierLine">
-        <div class="col-xs-12 col-lg-6 ">
-          <div class="atelierCard">
-              <div id="parallelogramPurR" style="border-right:solid 20px orange">
-                <div class="row textAtelier">
-                  <div class="col-12">
-                    <p class="atelier">ATELIER #3</p>
-                  </div>
-                </div>
-                <div class="row textAtelier">
-                  <div class="col-xs-12 col-sm-12 col-md-12 col-lg-9 col-xl-9">
-                    <p class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                  </div>
-                </div>
-                <div class="row linkSubscribeLine">
-                  <div class="col-12 linkSubscribe">
-                    <a href="#" class="">Inscrivez-vous</a>
-                  </div>
-                </div>
-              </div>
-              <div id="imageRight">
-              </div>
-          </div>
-        </div>
-        <div class="col-xs-12 col-lg-6 ">
-          <div class="atelierCard">
-              <div id="parallelogramOrL"  style="border-left:solid 20px purple">
-                <div class="row textAtelierR">
-                  <div class="col-12">
-                    <p class="atelierR">ATELIER #4</p>
-                  </div>
-                </div>
-                <div class="row textAtelierR">
-                  <div class="col-10">
-                    <p class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                  </div>
-                </div>
-                <div class="row linkSubscribeLine">
-                  <div class="col-12 linkSubscribeR">
-                    <a href="#" class="">Inscrivez-vous</a>
-                  </div>
-                </div>
-              </div>
-              <div id="imageRight">
-              </div>
-          </div>
-        </div>
-      </div>
-      <div class="row atelierLine">
-        <div class="col-xs-12 col-lg-6 ">
-          <div class="atelierCard">
-              <div id="parallelogramPurR">
-                <div class="row textAtelier">
-                  <div class="col-12">
-                    <p class="atelier">ATELIER #5</p>
-                  </div>
-                </div>
-                <div class="row textAtelier">
-                  <div class="col-xs-12 col-sm-12 col-md-12 col-lg-9 col-xl-9">
-                    <p class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                  </div>
-                </div>
-                <div class="row linkSubscribeLine">
-                  <div class="col-12 linkSubscribe">
-                    <a href="#" class="">Inscrivez-vous</a>
-                  </div>
-                </div>
-              </div>
-              <div id="imageRight">
-              </div>
-          </div>
-        </div>
-        <div class="col-xs-12 col-lg-6 ">
-          <div class="atelierCard">
-              <div id="parallelogramOrL">
-                <div class="row textAtelierR">
-                  <div class="col-12">
-                    <p class="atelierR">ATELIER #6</p>
-                  </div>
-                </div>
-                <div class="row textAtelierR">
-                  <div class="col-10">
-                    <p class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                  </div>
-                </div>
-                <div class="row linkSubscribeLine">
-                  <div class="col-12 linkSubscribeR">
-                    <a href="#" class="">Inscrivez-vous</a>
-                  </div>
-                </div>
-              </div>
-              <div id="imageRight">
-              </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </main>
-  <script src="assets/lib/bootstrap/js/bootstrap.js" type="text/javascript"></script>
-  <script src="assets/js/main.js" type="text/javascript"></script>
-</body>
-</html>
+      </footer>
+    </body>
+    <script src="assets/js/main.js" type="text/javascript"></script>
+    <script src="assets/js/bootstrap.js" type="text/javascript"></script>
+    </html>
